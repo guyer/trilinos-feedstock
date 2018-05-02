c25756d0285245f6fe775245d1088ded3083c32b
Jonathan Guyer <guyer@nist.gov>
2018-05-02 14:04:27 -0400

    Add Py3k builds?

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -41,18 +41,46 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 2 case(s).
+# Embarking on 6 case(s).
     set -x
     export CONDA_NPY=111
     export CONDA_PY=27
     set +x
-    conda build /recipe_root --quiet --prefix-length 80 || exit 1
+    conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
 
     set -x
     export CONDA_NPY=112
     export CONDA_PY=27
     set +x
-    conda build /recipe_root --quiet --prefix-length 80 || exit 1
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=112
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=36
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=112
+    export CONDA_PY=36
+    set +x
+    conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root guyer --channel=main || exit 1
 EOF
