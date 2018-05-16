bf6ae94371057795b3208a1725fc885386f99f1c
Jonathan Guyer <guyer@nist.gov>
2018-05-16 11:09:37 -0400

    Make recipe consistent with conda-forge/PyTrilinos-feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,39 +1,61 @@
 #!/bin/bash
 
-mkdir build
+mkdir -p build
 cd build
 
-# env LDFLAGS="-L/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/usr/lib/ $LDFLAGS" \
+if [ $(uname) == Darwin ]; then
+    export CXXFLAGS="-stdlib=libc++"
+fi
+
+export HYDRA_LAUNCHER=fork
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_MCA_rmaps_base_oversubscribe=yes
+
 cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
-    -D CMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
-    -D DART_TESTING_TIMEOUT:STRING=600 \
-    -D Trilinos_ENABLE_Fortran:BOOL=OFF \
-    -D Trilinos_ENABLE_TESTS:BOOL=ON \
-    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
-    -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
-    -D Trilinos_ENABLE_Epetra:BOOL=ON \
-    -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
-    -D Trilinos_ENABLE_AztecOO:BOOL=ON \
-    -D Trilinos_ENABLE_ML:BOOL=ON \
-    -D Trilinos_ENABLE_STK:BOOL=OFF \
-    -D TPL_ENABLE_MPI:BOOL=ON \
-    -D TPL_ENABLE_Netcdf=OFF \
-    -D MPI_BASE_DIR:PATH=${PREFIX} \
+    -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
     -D BUILD_SHARED_LIBS:BOOL=ON \
+    -D TPL_ENABLE_MPI:BOOL=ON \
+    -D MPI_BASE_DIR:PATH=${PREFIX} \
+    -D MPI_EXEC:FILEPATH=$PREFIX/bin/mpiexec \
+    -D MPI_EXEC_PRE_NUMPROCS_FLAGS:STRING="$MPI_FLAGS" \
+    -D PYTHON_EXECUTABLE:FILEPATH=$PYTHON \
     -D SWIG_EXECUTABLE:FILEPATH=$PREFIX/bin/swig \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
-    -D PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
-    -D PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}${SHLIB_EXT} \
+    -D DOXYGEN_EXECUTABLE:FILEPATH=$PREFIX/bin/doxygen \
+    -D Trilinos_ENABLE_Fortran:BOOL=OFF \
+    -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
+    -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF \
+    -D Trilinos_ENABLE_TESTS:BOOL=OFF \
+    -D Trilinos_ENABLE_EXAMPLES:BOOL=OFF \
+    -D Trilinos_ENABLE_Epetra:BOOL=ON \
+    -D Trilinos_ENABLE_Triutils:BOOL=ON \
+    -D Trilinos_ENABLE_Tpetra:BOOL=ON \
+    -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+    -D Trilinos_ENABLE_Domi:BOOL=ON \
+    -D Trilinos_ENABLE_Isorropia:BOOL=OFF \
+    -D Trilinos_ENABLE_Pliris:BOOL=OFF \
+    -D Trilinos_ENABLE_AztecOO:BOOL=ON \
+    -D Trilinos_ENABLE_Galeri:BOOL=ON \
+    -D Trilinos_ENABLE_Amesos:BOOL=ON \
+    -D Trilinos_ENABLE_Ifpack:BOOL=ON \
+    -D Trilinos_ENABLE_Komplex:BOOL=ON \
+    -D Trilinos_ENABLE_ML:BOOL=ON \
+    -D Trilinos_ENABLE_ML:BOOL=ON \
+    -D Trilinos_ENABLE_Anasazi:BOOL=ON \
+    -D Trilinos_ENABLE_NOX:BOOL=OFF \
+    -D NOX_ENABLE_LOCA:BOOL=OFF \
+    -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
+    -D Trilinos_ENABLE_PyTrilinos:BOOL=ON \
+    -D PyTrilinos_ENABLE_TESTS:BOOL=ON \
+    -D PyTrilinos_ENABLE_EXAMPLES:BOOL=ON \
     -D PyTrilinos_INSTALL_PREFIX:PATH=$PREFIX \
-    -D BLAS_LIBRARY_DIRS=${PREFIX}/lib \
-    -D BLAS_LIBRARY_NAMES="openblas;libgfortran.so.1" \
-    -D LAPACK_LIBRARY_DIRS=${PREFIX}/lib \
-    -D LAPACK_LIBRARY_NAMES="openblas" \
-    ..
+    $SRC_DIR
 
 make -j $CPU_COUNT
+
+ctest --output-on-failure
+
 make install
 
 
